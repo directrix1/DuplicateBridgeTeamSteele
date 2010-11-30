fd15b1ab79b04d11a522b0eba74e82c767f4e080
Edward Flick <directrix1@gmail.com>
2010-11-30 13:20:54 -0600

    Totally changed Mio, to conform to review.

--- code/modules/Mio.lisp
+++ code/modules/Mio.lisp
@@ -2,19 +2,21 @@
 ;; They tell DrScheme that this is a Dracula Modular ACL2 program.
 ;; Leave these lines unchanged so that DrScheme can properly load this file.
 #reader(planet "reader.ss" ("cce" "dracula.plt") "modular" "lang")
-(require "../interfaces/iio.lisp")
-(require "../interfaces/iboard.lisp")
+(require "../interfaces/Iio.lisp")
+(require "../interfaces/Iboard.lisp")
 (require "../interfaces/Ixmlminidom.lisp")
 
 (module mio
   (import iboard)
+  (import Ixmlminidom)
   
-  ;boards-no-trav(bridgeXML state) Given a duplicate bridge XML file, gets
+  (include-book "io-utilities" :dir :teachpacks)
+  
+  ;boards-no-trav (bntXML state) Given a duplicate bridge XML file, gets
   ;the following information from each board listed in the XML file and 
   ;creates an HTML page with this information formatted into tables:
   ;Board numbers, dealer, vulnerable, and hands for each direction
-  (defun boards-no-trav(bridgeXML state)
-    (defun boards-trav(bridgeXML state)
+  (defun boards-no-trav (bntXML state)
     (mv-let
      (status state)
      (string-list->file
@@ -23,22 +25,22 @@
        *htmlhead*
        (serializedboards 
         (xml-getnodes (xml-getnode (xml-getnode 
-                      (xml-readnode bridgeXML)
+                      bntXML
                       "Game") "HandRecords") "Board") '0)
        *htmltail*
        )
       state)
      (if (null status)
          (mv 'ok state)
-         (mv 'error state)))))
+         (mv 'error state))))
   
   
-  ;boards-trav(bridgeXML state) Given a duplicate bridge XML file, gets the
+  ;boards-trav (btXML state) Given a duplicate bridge XML file, gets the
   ;same information as boards-no-trav as well as the travelers information 
   ;for each board, which includes the total score and matchpoints for all 
   ;pairs at that board. Creates HTML page with this information with the 
   ;board information and travelers information in separate tables.
-  (defun boards-trav(bridgeXML state)
+  (defun boards-trav (btXML state)
     (mv-let
      (status state)
      (string-list->file
@@ -47,7 +49,7 @@
        *htmlhead*
        (serializedboards 
         (xml-getnodes (xml-getnode (xml-getnode 
-                      (xml-readnode bridgeXML)
+                      btXML
                       "Game") "HandRecords") "Board") '1)
        *htmltail*
        )
@@ -57,18 +59,18 @@
          (mv 'error state))))
   
   
-  ;rankings(bridgeXML state) Given a duplicate bridge XML file, creates a 
+  ;rankings (rnkXML state) Given a duplicate bridge XML file, creates a 
   ;rankings table in an HTML file including each pairs ranking and various 
   ;other stats such as matchpoint and percentage score.
-  (defun rankings(bridgeXML state)bridgeXML)
+  (defun rankings (rnkXML state) rnkXML)
   
   
-  ;personal-score-cards(bridgeXML state) Given a duplicate bridge XML file,
+  ;personal-score-cards (pscXML state) Given a duplicate bridge XML file,
   ;creates an HTML file containing personal score cards for each pair, which
   ;includes information about each match they played an against whom.
-  (defun personal-score-cards (bridgeXML state)bridgeXML)
+  (defun personal-score-cards (pscXML state) pscXML)
 
-  ;main(bridgeXML state) Given a duplicate bridge XML file, extracts
+  ;main (bridgeXML state) Given a duplicate bridge XML file, extracts
   ;appropriate information to create four 
   ;HTML pages that link together: boards, boards with travelers, rankings,
   ;and personal score card webpages.
@@ -76,13 +78,14 @@
     (mv-let (contents status state)
           (file->string (string-append bridgeXML ".xml") state)
           (if (null status)
-              (mv-let
-               (bt bnt rnk psc state)
-               (boards-trav contents)
-               (boards-no-trav contents)
-               (rankings contents)
-               (personal-score-cards contents)
+              (let* (
+               (xml (xml-readnode contents))
+               (bt (boards-trav xml state))
+               (bnt (boards-no-trav xml (cadr bt)))
+               (rnk (rankings xml (cadr bnt)))
+               (psc (personal-score-cards xml (cadr rnk)))
                )
+              (mv 'ok (cadr psc)))
               (mv 'error state))))
   
   (export iio))
