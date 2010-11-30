a44b2c72cf97bc79bbce81cf0d37e401adb3adbd
Micah Morgan <MNMorgan@OU.edu>
2010-11-30 14:46:32 -0600

    Basic structure for Mpsc.lisp

---
+++ code/modules/Mpsc.lisp
@@ -0,0 +1,44 @@
+#| Team Steele
+   Software Engineering I
+   Mpsc
+  
+   Personal Score Card Module
+|#
+(require "../interfaces/Ipsc.lisp")
+(require "../interfaces/Iboard.lisp")
+(require "../interfaces/Irankings.lisp")
+(require "../interfaces/Ixmlminidom.lisp")
+(require "../interfaces/Ibasiclex.lisp")
+
+(module Mpsc
+  (import Iboard)
+  (import Irankings)
+  (import Ibasiclex)
+  (import Ixmlminidom)
+  (include-book "io-utilities" :dir :teachpacks)
+  (include-book "list-utilities" :dir :teachpacks)
+  
+  ;Pulls the Personal Score Card data for all players, and put's them all
+  ;into html table format
+  ;XMLnodes format: Nodes format
+  ;Output format: String, HTML formatted text comprising the score card
+  ;    for one player pair
+  (defun getPSC(xmlnodes)
+    ())
+  
+  ;Pulls the Name Strings for a given Pair ID
+  ;PairID format: (String Direction, String SectionNumber)
+  ;Data format: Nodes format
+  ;Output format: (String String), Names of the two players
+  (defun getNameForID(pairid data)
+    ())
+  
+  ;Pulls the match results for a given Pair ID
+  ;PairID format: (String Direction, String SectionNumber)
+  ;Results format: ?
+  ;Output format: String, HTML formatted text comprising all the boards
+  ;    for one player pair
+  (defun getBoardsForPair(pairid results)
+    ())
+  
+  (export Ipsc))
