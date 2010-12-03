85f36ac2ae90c0f80b220104561fa32255ccbe8d
Colby Russell <spade@ruta-corsica.(none)>
2010-12-02 18:44:07 -0600

    Untested code for getcontestants, narrowing down to the correct section.

--- code/modules/Mrankings.lisp
+++ code/modules/Mrankings.lisp
@@ -12,11 +12,51 @@
   ; nodes, too.
   (defun getrankings (rankingnodes)
     nil)
-  ; Approach, use xml-getnodes on rankingnodes in a BFS fashion until we
+
+  ; Given a list of nodes, glue all nodes' children together in one big
+  ; list; i.e., if the nodes are rooted in some tree where they're at depth
+  ; k, then take all of the nodes at depth k+1 (cousins or siblings to one
+  ; another), and put them into a list together.
+  (defun gluekids (nodes)
+    (if (consp nodes)
+      (mv-let (nodename atts kids)
+              (car nodes)
+              (concatenate 'list kids (gluekids (cdr nodes))))
+      nil))
+
+  (defun findsectionnodes (nodes)
+    ; Build a dummy node that looks like xmlminidom, so we can act like
+    ; nodes are rooted there as children, and we can just use xml-getnodes
+    ; on it.
+    (let* ((dummyroot (mv 'dummyroot nil nodes))
+           (maybesections (xml-getnodes dummyroot 'Section)))
+        (if maybesections
+            ; We found them
+            maybesections
+            ; There were no Section nodes.  Look deeper.
+            (findsectionnodes (gluekids nodes)))))
+  
+  ; sectionnodes should be a list of section nodes
+  (defun findspecificsection (sectionnodes label dir)
+    (if sectionnodes
+      ; linear search!
+      (let* ((current (car sectionnodes)))
+        (if (and (equal (xml-getattribute current "SectionLabel")
+                        label)
+                 (equal (xml-getattribute current "Direction")
+                        dir))
+            current
+            (findspecificsection (cdr sectionnodes) label dir)))
+      nil))
+
+  ; Approach: use xml-getnodes on rankingnodes in a BFS fashion until we
   ; hit the Section nodes.  Start trying to find the Section node whose
   ; SectionLabel and Direction attributes match section and dir,
   ; respectively.  Use xml-getnodes for Contestants on that section, and
   ; find the Contestants node whose ID attribute matches id....
   (defun getcontestants (section dir id rankingnodes)
-    nil)
+    (let* ((section (findspecificsection (findsectionnodes rankingnodes)
+                                         section
+                                         dir)))
+      nil))
   (export Irankings))
